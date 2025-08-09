b6ce371c083b0e7173007a99ee6eb9fec4cc26f7
Everth Paredes <epe0413@gmail.com>
2025-08-09 17:42:43 -0600

    Variables y constantes

---
+++ src/bases/01-const-let.ts
@@ -0,0 +1,10 @@
+const firstName: string = 'Everth';
+const lastName: string = 'Paredes';
+
+let diceNumber: number = 5;
+diceNumber = 3;
+// diceNumber = '3'
+
+const containsLetterE = lastName.includes('E');
+
+console.log({containsLetterE, diceNumber, firstName, lastName});
