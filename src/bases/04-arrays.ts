636c04914c811d96a2bfba54199eedb8e983e1d4
Everth Paredes <epe0413@gmail.com>
2025-08-11 23:13:46 -0600

    Arreglos

---
+++ src/bases/04-arrays.ts
@@ -0,0 +1,17 @@
+
+
+const myArray: number[] = [1,2,3,4,5,6];
+const myArray2 = [...myArray]
+
+myArray.push(10);
+myArray.push(11);
+
+// console.log(myArray);
+
+for (const myNumber of myArray){
+    console.log(myNumber + 10)
+}
+
+myArray2.push(7)
+
+console.log({ myArray, myArray2})
