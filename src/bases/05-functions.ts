41e946cc5beb6da59ea9295add2e85f565b58758
Everth Paredes <epe0413@gmail.com>
2025-08-11 23:53:08 -0600

    Funciones con múltiples retornos

--- src/bases/05-functions.ts
+++ src/bases/05-functions.ts
@@ -5,29 +5,47 @@ function greet( name: string ): string {
     // return 123
 }
 
-const greet2 = (name: string): string => {
-    return `Hola ${name}`
-}
+const greet2 = (name: string): string => `Hola ${name}`
+
 
 const message = greet('Goku')
 const message2 = greet2('Vegeta')
 
 console.log(message, message2)
 
-function getUser(){
+interface User {
+    uid: string;
+    username: string;
+}
+
+function getUser(): User {
     return {
         uid: 'ABC-123',
         username: 'El_Papi23'
     };
 }
 
-const getUser2 = () => {
-    return {
+const getUser2 = (): User => ({
         uid: 'ABC-123',
         username: 'El_Papi23'
-    };
-}
+    });
 
 const user = getUser()
 const user2 = getUser2()
 console.log(user, user2);
+
+const myNumbers = [1,2,3,4,5];
+
+// myNumbers.forEach( function(value) {
+//     console.log({ value })
+// })
+
+// myNumbers.forEach((value) => {
+//     console.log(value)
+// })
+
+// myNumbers.forEach(console.log)
+
+myNumbers.forEach((value, index, arr) => {
+    console.log(value, index, arr)
+})
