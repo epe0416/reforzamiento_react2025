4a5b03eb17ca837b47e2824f62d7646715221801
Everth Paredes <epe0413@gmail.com>
2025-08-13 22:37:24 -0600

    Tarea - Desestructuración

--- src/bases/07-array-destructuring.ts
+++ src/bases/07-array-destructuring.ts
@@ -4,7 +4,7 @@ const characterNames = ['Goku', 'Vegeta', 'Trunks'];
 // const[p1,p2,p3] = characterNames
 // const[,p2] = characterNames
 const[, ,p3] = characterNames
-console.log({p3})
+// console.log({p3})
 
 const returnsArrayFn = () => {
     return['ABC', 123] as const;
@@ -12,5 +12,21 @@ const returnsArrayFn = () => {
 
 const [ letters, numbers ] = returnsArrayFn();
 
-console.log( numbers + 100 )
-console.log( letters + 100 )
+// console.log( numbers + 100 )
+// console.log( letters + 100 )
+
+
+// Tarea
+
+const useState = (value: string) => {
+    return[
+        value,
+        (newValue: String) => {
+            console.log(newValue)
+        },
+    ] as const;
+};
+
+const[ name, setName ] = useState('Goku')
+console.log(name)
+setName('Vegeta')
