b77d18f695cf6de58bae51d695a28e31930ba5c3
Everth Paredes <epe0413@gmail.com>
2025-08-14 23:22:29 -0600

    Async - Await

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import './style.css'
-import './bases/10-fetch-api'
-import { getHeroesByOwner } from './bases/08-imp-exp'
-import { Owner } from './data/heroes.data'
+import './bases/11-async-await'
+// import { getHeroesByOwner } from './bases/08-imp-exp'
+// import { Owner } from './data/heroes.data'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
